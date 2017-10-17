cdaf5945da4279111518f0f42c1fba29f308b961
Jakub Chmiel <jakub.chmiel@roche.com>
2017-10-17 14:33:27 +0200

    Knowledge Base data + reader

---
+++ knowledge_base/KnowledgeBaseReader.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+KNOWLEDGE_BASE_PATH = os.path.dirname(__file__)
+KNOWLEDGE_BASE_FILE = os.path.join(KNOWLEDGE_BASE_PATH, "main.csv")
+
+
+def get_kb_extract():
+    return pd.read_csv(KNOWLEDGE_BASE_FILE)
+
+if __name__ == "__main__":
+    print(get_kb_extract())
