544cab784e2154788769806535c9222b5830760c
Jakub Chmiel <jakub.chmiel@roche.com>
2017-10-18 09:50:46 +0200

    generate answer for ask_property

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import make_response
 from flask import request
 import json
-
+from knowledge_base import KnowledgeBaseReader
 app = Flask(__name__)
 
 
@@ -18,7 +18,12 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     print('Request:\n{0}'.format(json.dumps(req, indent=4)))
 
-    response = prepare_response("chat bot test speech")
+    result = req.get("result").get("parameters")
+    property = result.get("property")
+    element = result.get("element")
+
+    answer = prepare_answer(element, property)
+    response = prepare_response(answer)
 
     response = json.dumps(response, indent=4)
     print('Response:\n{0}'.format(response))
@@ -37,6 +42,11 @@ def prepare_response(answer, context_out=None):
 
     return response
 
+
+def prepare_answer(element, property):
+    kb = KnowledgeBaseReader.get_kb_extract()
+    property_value = kb[kb["Element"] == element][property].tolist()[0]
+    return "The {0} of {1} is {2}.".format(property, element, property_value)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
--- knowledge_base/KnowledgeBaseReader.py
+++ knowledge_base/KnowledgeBaseReader.py
@@ -9,4 +9,12 @@ def get_kb_extract():
     return pd.read_csv(KNOWLEDGE_BASE_FILE)
 
 if __name__ == "__main__":
-    print(get_kb_extract())
+    # print(get_kb_extract())
+
+
+    kb = get_kb_extract()
+    ELEMENT = 'Carbon'
+    PROPERTY = 'Density'
+
+    print kb[kb["Element"] == ELEMENT][PROPERTY].tolist()[0]
+
