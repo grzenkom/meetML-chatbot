1781478baaa4198c1b21e18bf8bdbdd4c9860f52
Jakub Chmiel <jakub.chmiel@roche.com>
2017-10-18 14:31:14 +0200

    abundance condition added

--- helpers/AnswerGenerator.py
+++ helpers/AnswerGenerator.py
@@ -3,7 +3,7 @@ from knowledge_base import KnowledgeBaseReader
 
 COLUMN_MAPPING = {
     "atomic weight": {"column_name": "Atomic_weight", "unit": "u"},
-    "density": {"column_name": "Density", "unit": "g/cm3"},
+    "density": {"column_name": "Density", "unit": "g/cm³"},
     "melting point": {"column_name": "Melting_point", "unit": "K"},
     "boiling point": {"column_name": "Boiling_point", "unit": "K"},
     "specific heat capacity": {"column_name": "Specific_heat_capacity", "unit": "J/g · K"},
@@ -12,10 +12,20 @@ COLUMN_MAPPING = {
     "period": {"column_name": "Period", "unit": ""},
     "group": {"column_name": "Group", "unit": ""},
     "origin of name": {"column_name": "Origin_of_name", "unit": ""},
-    "abundance": {"column_name": "Abundance_in_Earths_crust", "unit": "mg/kg"},
+    "abundance": {"column_name": "Abundance_in_Earths_crust", "unit": ""},
 }
 
 ANSWER_TEMPLATE = "The {0} of {1} is {2}{3}."
+ABUNDANCE_THRESHOLD = 0.01
+
+
+def abundance_value_mapping(action, value):
+    if action == "ask_abundance":
+        if float(value) >= ABUNDANCE_THRESHOLD:
+            return "quite common"
+        else:
+            return "rare"
+    return value
 
 
 def prepare_answer(action, parameters):
@@ -35,6 +45,7 @@ def prepare_answer(action, parameters):
     if unit:
         unit = " " + unit
     value = KnowledgeBaseReader.get_kb_value(element, column_name)
+    value = abundance_value_mapping(action, value)
 
     return ANSWER_TEMPLATE.format(column_mapping_key, element, value, unit)
 
